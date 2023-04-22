dde67663873efae19d3a36d5f968765beaf5a992
mnj2kk <cryatbathroom@gmail.com>
2023-04-22 22:39:00 +0300

    add endpoint to update process

--- app/main.py
+++ app/main.py
@@ -20,3 +20,22 @@ async def get_result():
     if process.last_status == -1:
         return '404 Not Found'
     return process.last_status
+
+@app.post("/api/sleep_process")
+async def update_process(body: str = Body(...)):
+    global process_thread
+    match body:
+        case 'start':
+            if not process.status():
+                process_thread = update_thread()
+                process_thread.start()
+                return 'Process started.'
+            return 'Process is already running.'
+        case 'stop':
+            if not process.status():
+                return 'Procces has already been stopped.'
+            process.kill()
+            process_thread = update_thread()
+            return 'Process stopped.'
+        case _:
+            return 'Invalid body %s, for POST /api/sleep_process.' % body
