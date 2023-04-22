c021ea793e2e96f42de2bacedd368403005ea878
mnj2kk <cryatbathroom@gmail.com>
2023-04-22 22:35:07 +0300

    fix update_thread and add result endpoint

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 process = ProcessHandler()
 
-async def update_thread():
+def update_thread():
     return Thread(target=process.run)
 
 process_thread = update_thread()
@@ -14,3 +14,9 @@ process_thread = update_thread()
 @app.get("/api/sleep_process")
 async def get_status():
     return 'Procces is running.' if process.status() else 'Process is not running.'
+
+@app.get("/api/sleep_process/result")
+async def get_result():
+    if process.last_status == -1:
+        return '404 Not Found'
+    return process.last_status
